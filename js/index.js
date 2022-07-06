09c4e09ebbdd9642ab58b1b755580f7024d01d14
Clara Pons <clarapons@air-de-clara.home>
2022-07-06 17:30:51 +0200

    first task JS

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
+let addButton = document.getElementById("addButton")
+
 const tasks = []
+
+const taskInfos = {
+	value: "",
+	status: "to do",
+}
+
+const onTaskSubmit = (e) =>{
+    e.preventDefault()
+    let x = document.getElementById("task-push").value
+
+    console.log(onTaskSubmit)
+}
+
+addButton.addEventListener("click", onTaskSubmit)
 
 let modifyData = () => {
     
-}
-
-
-const OnTaskSubmit = () =>{
-
 }
 
