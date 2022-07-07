841e577dc19bce73b1d47f89a8475d0c7ab73190
Clara Pons <clarapons@air-de-clara.home>
2022-07-07 17:33:30 +0200

    js mod

--- js/index.js
+++ js/index.js
@@ -7,59 +7,61 @@ let allLink = document.getElementById("allLink")
 const tasks = []
 
 
+
 // on ajoute une tâche
 const onTaskSubmit = (e) =>{
     e.preventDefault()
-    let x = document.getElementById("task-push").value
+    let input = document.getElementById("task-push").value
     
     const taskInfos = {
-        value: x,
+        value: input,
         status: "to do",
+        id: Date.now(),
     }
 
-    let arrayX = tasks.push(taskInfos)
+    tasks.push(taskInfos)
     
     console.log(tasks)
 
-       let test = document.getElementById("task")
-       let newTasks = test.innerHTML +=
-        ` <div id="task-${Date.now}">
-        <div id="checkbox-${Date.now}"></div>
-        <form id="form-task-${Date.now}">
-            <input type="text" id="input-task-${Date.now}" name="input-task" value="${x}">
-        </form>
+       let taskContainer = document.getElementById("task")
+       taskContainer.innerHTML = ""
+       tasks.forEach (element => {
+       taskContainer.innerHTML +=
+        ` <div id="task-${element.id}">
+        <div id="checkbox-${element.id}"></div>
+            <input type="text" id="task-creation-${element.id}" name="creation" value="${element.value}">
         <div id="img-modify-carbage">
-            <img id ="modify-${Date.now}" src="./img/edit.png" alt="modify" />
-            <img id = "delete-${Date.now}" src="./img/trash.png" alt="delete" />
+            <img id ="modify-${element.id}" src="./img/edit.png" alt="modify" onClick="onModify('task-${element.id}')"/>
+            <img id = "delete-${element.id}" src="./img/trash.png" alt="delete" onClick="onDelete('task-${element.id}')" />
         </div>
         <div id="update">
-            <div id="title-status-${Date.now}">
+            <div id="title-status-${element.id}">
                 <img src="./img/edit.png" alt="modify" />
                 <p>Status update</p>
             </div>
             <form>
-                <input type="text" id="modification-${Date.now}" name="first-modification" value="">
-                <select name="status" id="status-select-${Date.now}">
+                <input type="text" id="modification-${element.id}" name="first-modification" value="${element.value}">
+                <select name="status" id="status-select-${element.id}">
                     <option value="">Please update your status</option>
                     <option value="Todo">To Do</option>
                     <option value="Doing">Doing</option>
                     <option value="Done">Done</option>
                 </select>
-                <input id="submit--${Date.now}" type="submit" value="Modifier">
+                <input id="submit--${element.id}" type="submit" value="Modifier">
             </form>
         </div>  
     </div>
-        `
-        
+     `      
+     
+    })
 }
-
 
 console.log(onTaskSubmit)
 
 addButton.addEventListener("click", onTaskSubmit)
 
-let modifyData = () => {
-    
+let onModify = (id) => {
+    console.log(id)
 }
 
 // filtrer les taches 
@@ -71,6 +73,10 @@ let modifyData = () => {
 
 
 //  delete 
+
+let onDelete = (id) => {
+    console.log(task-id)
+}
 
 // toDoLink.addEventListener("click", event => {
 //     task.remove()
