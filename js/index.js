4ec3696de70602dd705223a1d725431cefb8aafc
Clara Pons <clarapons@MacBook-Air-de-Clara.local>
2022-07-10 19:26:41 +0200

    filter

--- js/index.js
+++ js/index.js
@@ -33,6 +33,7 @@ const onTaskSubmit = (e) =>{
         `   <div id="task-${element.id}">
                 <div id="checkbox-${element.id}"></div>
                 <input type="text" id="task-creation-${element.id}" name="creation" value="${element.value}">
+                <b class="status-${element.status}">${element.status}</b>
                 <div id="img-modify-carbage" class = "delete">
                         <img  src="./img/edit.png" alt="modify" onCLick="onModify(${element.id})"/>
                         <img src="./img/trash.png" alt="delete" onCLick="onDelete(${element.id})"/>
@@ -54,6 +55,9 @@ const display = (element => {
         `   <div id="task-${element.id}">
                 <div id="checkbox-${element.id}"></div>
                 <input type="text" id="task-creation-${element.id}" name="creation" value="${element.value}">
+                <div class="col-2 d-flex align-items-center">
+                <b class="status-${element.status}">${element.status}</b>
+              </div>
                 <div id="img-modify-carbage" class = "delete">
                         <img  src="./img/edit.png" alt="modify" onCLick="onModify(${element.id})"/>
                         <img src="./img/trash.png" alt="delete" onCLick="onDelete(${element.id})"/>
@@ -77,27 +81,34 @@ let onModify = (index) =>{
     let task = tasks[index2]
     
     taskModify.innerHTML =
-            `   <div class="row justify-between py-2">
-            <div class="col-5 d-flex align-items-center">
-              <input id="item-${index}-input" type="text" class="form-control" value="${task.value}" autofocus />
+            `   <div class="update">
+            <div class="form-modif">
+              <input id="item-${index2}-input" type="text" class="form-control" value="${task.value}" autofocus />
             </div>
-            <div class="col-2 d-flex align-items-center">
-              <select id="item-${index}-select" class="form-select" aria-label="Select status">
+            <div class="status">
+              <select id="item-${index2}-select" class="form-select" aria-label="Select status">
                 <option ${task.status === 'to do' ? 'selected' : ''} value="to do">To do</option>
                 <option ${task.status === 'doing' ? 'selected' : ''} value="doing">Doing</option>
                 <option ${task.status === 'done' ? 'selected' : ''} value="done">Done</option>
               </select>
             </div>
-            <div class="col-2 d-flex align-items-center">
-              <input id="item-${index}-priority" type="number" class="form-control" value="${task.priority}" min="0" max="5" />
-            </div>
             <div class="col-3 d-flex justify-content-end">
               <button type="button" class="btn btn-outline-danger me-3" onclick="display()">Annuler</button>
-              <button type="button" class="btn btn-outline-success" onclick="modification(${index})">Valider</button>
+              <button type="button" class="btn btn-outline-success" onclick="modification(${index2})">Valider</button>
             </div>
           </div> 
   `
+  
+}
 
+let modification = (index) => {
+    let value = document.getElementById(`item-${index}-input`)
+    let status = document.getElementById(`item-${index}-select`)
+    tasks[index].value = value.value
+    tasks[index].status = status.value
+    display()
+
+    console.log(tasks   )
 }
 
 //  delete 
@@ -117,46 +128,19 @@ let onDelete = (number) => {
 
 // filtrer 
 
-let toDoTaskList = (toDo) => {
-    tasks.filter((task) => {
+
+let toDoTaskList = (status) => {
+    let taskFilter = tasks.filter((task) => {
     return task.status === "to do"
 })
+display()
 }
 
-toDoLink.addEventListener("click", toDoTaskList)
 
-display(toDoTaskList)
-
-// {/* <div id="update">
-//                 <div id="title-status-${element.id}">
-//                     <img src="./img/edit.png" alt="modify" />
-//                     <p>Status update</p>
-//                 </div>
-//                 <form>
-//                     <input type="text" id="modification-${element.id}" name="first-modification" value="${element.value}">
-//                     <select name="status" id="status-select-${element.id}">
-//                         <option value="">Please update your status</option>
-//                         <option value="Todo">To Do</option>
-//                         <option value="Doing">Doing</option>
-//                         <option value="Done">Done</option>
-//                     </select>
-//                     <input id="submit--${element.id}" type="submit" value="Modifier">
-//                 </form>
-//             </div>   */}
-
-// console.log(onTaskSubmit)
+toDoLink.addEventListener("change", toDoTaskList)
 
 
-// let onModify = (id) => {
-//     console.log(id)
-// }
-
-// filtrer les taches 
-
-// toDoLink.addEventListener("click", event => {
-//     const filterToDo = tasks.filter()
-// })
-// 
+ 
 
 
 
